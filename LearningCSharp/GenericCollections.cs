494abcb954a93dd551088050e0be8a2faeddf04a
Ryan Yoak <ryan.yoak@gmail.com>
2021-03-16 15:05:55 -0400

    A bunch of stuff

---
+++ LearningCSharp/GenericCollections.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LearningCSharp
+{
+    public class GenericCollections
+    {
+        [Fact]
+        public void FavoriteNumbersAgain()
+        {
+            // Parametric Polymorphism <int>
+            var favoriteNumbers = new List<int>();
+            favoriteNumbers.Add(42);
+            favoriteNumbers.Add(20);
+            var firstTwo = favoriteNumbers[0] + favoriteNumbers[1];
+            Assert.Equal(62, firstTwo);
+        }
+
+        [Fact]
+        public void UsingADictionary()
+        {
+            var mealPlan = new Dictionary<string, Dinner>();
+            mealPlan.Add("Tuesday", new Dinner()
+            {
+                MainCourse = "Pizza",
+                HasSides = true,
+            });
+            mealPlan.Add("Wednesday", new Dinner()
+            {
+                MainCourse = "Pasta"
+            });
+
+            Assert.Equal("Pizza", mealPlan["Tuesday"].MainCourse);
+        }
+
+        [Fact]
+        public void CollectionInitializers()
+        {
+            var favoriteNumbers = new List<int> { 
+                1, 42, 67, 4643, 236, 23643, 264, 267, 26, 26, 23654
+            };
+
+            Assert.Equal(42, favoriteNumbers[1]);
+
+            var friends = new Dictionary<char, string>
+            {
+                {'s', "Sean Carlin" },
+                {'r', "Ryan Yoak" },
+                {'j', "Jarrett Soross" },
+                {'c', "Charlie Himmers" }
+            };
+
+            Assert.Equal("Ryan Yoak", friends['r']);
+            Assert.True(friends.ContainsKey('r'));
+            Assert.False(friends.ContainsKey('x'));
+        }
+
+        [Fact]
+        public void EnumeratingCollection()
+        {
+            // Enumerating means "Going through one at a time with a goal"
+            var favoriteNumbers = new List<int> {
+                1, 42, 67
+            };
+
+            var friends = new Dictionary<char, string>
+            {
+                {'s', "Sean Carlin" },
+                {'r', "Ryan Yoak" },
+                {'j', "Jarrett Soross" },
+                {'c', "Charlie Himmers" }
+            };
+
+            var total = 0;
+            foreach(var number in favoriteNumbers)
+            {
+                total += number;
+            }
+
+            Assert.Equal(110, total);
+        }
+    }
+}
---
+++ LearningCSharp/OldSchoolCollections.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LearningCSharp
+{
+    public class OldSchoolCollections
+    {
+        // DO NOT USE THESE IN ACTUAL CODE
+        [Fact]
+        public void FavoriteNumbersList()
+        {
+            // This only takes objects in it, and treats it like objects
+            // favoriteNumbers[1] + favoriteNumbers[0] won't compile
+            // as object + object won't work
+            // Also this is SLOW
+            var favoriteNumbers = new ArrayList();
+            favoriteNumbers.Add(9);
+            favoriteNumbers.Add(20);
+            favoriteNumbers.Add(42);
+            favoriteNumbers.Add(108);
+
+            Assert.Equal(9, favoriteNumbers[0]);
+            Assert.Equal(108, favoriteNumbers[3]);
+        }
+    }
+}
